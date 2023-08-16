e8c1b0bf025d75c5c51d8e895124abc90f80e5f9
Rishi <rishivhavle21@gmail.com>
2023-08-16 06:05:48 +0530

    major ui changes

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -8,7 +8,7 @@ function Topbar() {
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
         <Image src='/assets/favicon.ico' alt='logo' width={35} height={35} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>SigmaGram</p>
+        <p className='text-heading3-bold text-light-1'>SigmaGram</p>
       </Link>
       
 
