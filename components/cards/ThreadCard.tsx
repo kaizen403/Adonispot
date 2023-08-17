fb681ba1a17f8305ad5fd7e753c721034313c7f0
Rishi <rishivhavle21@gmail.com>
2023-08-17 14:21:52 +0530

    hello

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -48,7 +48,7 @@ function ThreadCard({
       <div className='flex items-start justify-between'>
         <div className='flex w-full flex-1 flex-row gap-4'>
           <div className='flex flex-col items-center'>
-            <Link href={`/profile/${author.id}`} className='relative h-11 w-11'>
+              <Link href={`/profile/${author.id}`} className='relative h-11 w-11'>
               <Image
                 src={author.image}
                 alt='user_community_image'
