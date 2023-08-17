3647408f55b4dd46d4afc85778dc831cdbaa44b5
Rishi <rishivhavle21@gmail.com>
2023-08-17 14:35:36 +0530

    hello

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
           <h1 className={`text-white tracking-wide font-extrabold ${lexend.className}`} style={{ fontSize: '50px' }}>Welcome to SigmaGram</h1>
           <p className={`mt-5 text-white tracking-normal leading-loose ${lexend.className}`} style={{ fontSize:'20px'}}>Seeking a space dedicated to self-improvement and growth? Look no further! Connect with like-minded individuals who are on their journey to becoming the best versions of themselves. Engage in healthy competition, draw inspiration from those ahead of you, and let that motivation fuel your desire to surpass your limits. Elevate your potential in an environment where everyone strives for excellence. Let's grow together!</p>
         </div>
-        <div className="p-5 mb-6 lg:w-1/2 flex justify-center items-center">
+        <div className="pl-5 pt-1 pr-5 mb-6 lg:w-1/2 flex justify-center items-center">
           <SignIn />
         </div>
       </div>
