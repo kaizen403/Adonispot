2d25dffcd1f12a5886fbce3abb2da825bf01a60a
Rishi <rishivhavle21@gmail.com>
2023-08-17 14:34:04 +0530

    hello

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -8,9 +8,9 @@ export default function Page() {
       <div className="flex flex-col lg:flex-row justify-between items-center h-screen lg:h-auto">
         <div className="p-5 lg:w-1/2">
           <h1 className={`text-white tracking-wide font-extrabold ${lexend.className}`} style={{ fontSize: '50px' }}>Welcome to SigmaGram</h1>
-          <p className={`mt-5 text-white tracking-wide leading-loose ${lexend.className}`} style={{ fontSize:'20px'}}>Seeking a space dedicated to self-improvement and growth? Look no further! Connect with like-minded individuals who are on their journey to becoming the best versions of themselves. Engage in healthy competition, draw inspiration from those ahead of you, and let that motivation fuel your desire to surpass your limits. Elevate your potential in an environment where everyone strives for excellence. Let's grow together!</p>
+          <p className={`mt-5 text-white tracking-normal leading-loose ${lexend.className}`} style={{ fontSize:'20px'}}>Seeking a space dedicated to self-improvement and growth? Look no further! Connect with like-minded individuals who are on their journey to becoming the best versions of themselves. Engage in healthy competition, draw inspiration from those ahead of you, and let that motivation fuel your desire to surpass your limits. Elevate your potential in an environment where everyone strives for excellence. Let's grow together!</p>
         </div>
-        <div className="p-5 lg:w-1/2 flex justify-center items-center">
+        <div className="p-5 mb-6 lg:w-1/2 flex justify-center items-center">
           <SignIn />
         </div>
       </div>
