61aa6ac5c854982fbe6575780eda8deed052588c
Rishi <rishivhavle21@gmail.com>
2023-08-17 13:05:15 +0530

    laoding

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -3,6 +3,8 @@ import { LoadingSkeleton } from "@/components/shared/LoadingSkeleton";
 export default function Loading() {
     return <div className="flex justify-center items-center">
         <LoadingSkeleton/>
-        <h1 className="text-white text-2xl">Loading...</h1>
+        <div>
+        <h1 className="text-white mt-6 tracking-wide font-semibold text-2xl">Loading...</h1>
+        </div>
     </div>;
 }
