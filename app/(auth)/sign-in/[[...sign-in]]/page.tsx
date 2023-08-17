82571d1816b895cd3102f9a62295ba3d7196f380
Rishi <rishivhavle21@gmail.com>
2023-08-17 14:31:24 +0530

    hello

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,19 @@
 import { SignIn } from "@clerk/nextjs";
 import { Lexend } from "next/font/google";
 const lexend = Lexend({subsets: ['latin']})
+
 export default function Page() {
-  return <>
-  <div className="lg:flex justify-between">
-    <div className="ml-10 lg:w-1/2">
-      <h1 className={`text-white tracking-wide font-extrabold ${lexend.className}`} style={{ fontSize: '50px' }}>Welcome to SigmaGram</h1>
-      <div className="lg:hidden sm:mb-15 w-full flex justify-center"><SignIn /></div>
-      <p className={`mt-10 text-white tracking-wide leading-loose ${lexend.className}`} style={{ fontSize:'20px'}}>Seeking a space dedicated to self-improvement and growth? Look no further! Connect with like-minded individuals who are on their journey to becoming the best versions of themselves. Engage in healthy competition, draw inspiration from those ahead of you, and let that motivation fuel your desire to surpass your limits. Elevate your potential in an environment where everyone strives for excellence. Let's grow together!</p>
-      
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row justify-between items-center h-screen lg:h-auto">
+        <div className="p-5 lg:w-1/2">
+          <h1 className={`text-white tracking-wide font-extrabold ${lexend.className}`} style={{ fontSize: '50px' }}>Welcome to SigmaGram</h1>
+          <p className={`mt-5 text-white tracking-wide leading-loose ${lexend.className}`} style={{ fontSize:'20px'}}>Seeking a space dedicated to self-improvement and growth? Look no further! Connect with like-minded individuals who are on their journey to becoming the best versions of themselves. Engage in healthy competition, draw inspiration from those ahead of you, and let that motivation fuel your desire to surpass your limits. Elevate your potential in an environment where everyone strives for excellence. Let's grow together!</p>
+        </div>
+        <div className="p-5 lg:w-1/2 flex justify-center items-center">
+          <SignIn />
+        </div>
       </div>
-    <div className="lg:w-1/2 sm:hidden"><SignIn /></div>
-  </div>
-  </>
+    </>
+  );
 }
