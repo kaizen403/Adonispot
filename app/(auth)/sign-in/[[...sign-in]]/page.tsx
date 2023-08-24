b6a54b335e8eef8b942017cebc32ce38efa25a6b
Rishi <rishivhavle21@gmail.com>
2023-08-24 06:46:11 +0530

    sigin page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
       <div className="flex flex-col lg:flex-row justify-between pt-10 items-center h-screen lg:h-auto">
         <div className="pt-5 pl-5 pr-5 pb-2 lg:w-1/2">
           <h1 className={`text-white tracking-wide font-extrabold ${lexend.className}`} style={{ fontSize: '50px' }}>Welcome to SigmaGram</h1>
-          <p className={`mt-5 text-white tracking-normal lg:leading-loose ${lexend.className}`} style={{ fontSize:'20px'}}>Threads For Sigmas</p>
+          <p className={`mt-5 text-white tracking-normal lg:leading-loose ${lexend.className}`} style={{ fontSize:'20px'}}>My project SigmaGram provides a unique platform tailored for the introspective leaders, allowing them to engage in meaningful connections, content, and personal growth. It's a social platform not for the masses, but for depth-seekers.</p>
         </div>
         <div className="pl-5 pr-5 pb-7 lg:w-1/2 flex justify-center items-center">
           <SignIn />
